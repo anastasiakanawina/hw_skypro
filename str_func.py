708da276ed0c816b00a610eaed4ce322c3d6c301
avkanavina <a.kanavina@skyeng.ru>
2023-07-02 15:42:18 +0500

    edit str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def upper_func(new_string):
+	""" Приведение всех слов к верхнему регистру """
 	return new_string.upper()
+
+def title_func(new_string):
+	""" Приведение первых букв в слове к верхнему регистру """
+	return new_string.title()
